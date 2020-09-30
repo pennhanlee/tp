f6efdca3ffe04ebf63c5d47b87af7bb4304acb0f
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-09-30 23:03:26 +0800

    Add Prefixes for TotalPages and Bookmark

--- src/main/java/seedu/bookmark/logic/parser/CliSyntax.java
+++ src/main/java/seedu/bookmark/logic/parser/CliSyntax.java
@@ -9,5 +9,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_GENRE = new Prefix("g/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_TOTAL_PAGES = new Prefix("tp/");
+    public static final Prefix PREFIX_BOOKMARK = new Prefix("b/");
 
 }
