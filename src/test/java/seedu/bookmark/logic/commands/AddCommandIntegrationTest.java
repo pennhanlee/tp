c2bc42294d0f01297337c5f6886a7bb36b543d61
Penn Han <leepennhan98@gmail.com>
2020-10-05 16:40:25 +0800

    Edit MESSAGE_DUPLICATE_PERSON to MESSAGE_DUPLICATE_BOOK

--- src/test/java/seedu/bookmark/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/bookmark/logic/commands/AddCommandIntegrationTest.java
@@ -39,7 +39,7 @@ public class AddCommandIntegrationTest {
     @Test
     public void execute_duplicatePerson_throwsCommandException() {
         Book bookInList = model.getLibrary().getBookList().get(0);
-        assertCommandFailure(new AddCommand(bookInList), model, AddCommand.MESSAGE_DUPLICATE_PERSON);
+        assertCommandFailure(new AddCommand(bookInList), model, AddCommand.MESSAGE_DUPLICATE_BOOK);
     }
 
 }
--- src/test/java/seedu/bookmark/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/bookmark/logic/commands/AddCommandTest.java
@@ -47,7 +47,7 @@ public class AddCommandTest {
         AddCommand addCommand = new AddCommand(validBook);
         ModelStub modelStub = new ModelStubWithPerson(validBook);
 
-        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_PERSON, () -> addCommand.execute(modelStub));
+        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_BOOK, () -> addCommand.execute(modelStub));
     }
 
     @Test
