c1607a5c3eef2f7779b3dcc41f6ca763babb0a30
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-10-06 19:25:19 +0800

    Rename javadoc and constants in ListCommand

--- src/main/java/seedu/bookmark/logic/commands/ListCommand.java
+++ src/main/java/seedu/bookmark/logic/commands/ListCommand.java
@@ -6,13 +6,13 @@ import static seedu.bookmark.model.Model.PREDICATE_SHOW_ALL_BOOKS;
 import seedu.bookmark.model.Model;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all books in the library to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all books";
 
 
     @Override
