7913395e0482f344e7014774ea536f5a5619cec3
mgiang2015 <mgiang2015@gmail.com>
2020-10-07 23:21:19 +0800

    Fix checkStyle for view command

--- src/main/java/seedu/bookmark/logic/commands/ViewCommand.java
+++ src/main/java/seedu/bookmark/logic/commands/ViewCommand.java
@@ -1,16 +1,15 @@
 package seedu.bookmark.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
 import seedu.bookmark.commons.core.Messages;
 import seedu.bookmark.commons.core.index.Index;
 import seedu.bookmark.logic.commands.exceptions.CommandException;
 import seedu.bookmark.model.Model;
 import seedu.bookmark.model.book.Book;
 import seedu.bookmark.model.book.IsSpecifiedBookPredicate;
-
-import java.util.List;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.bookmark.model.Model.PREDICATE_SHOW_ALL_BOOKS;
 
 /**
  * Allows user to view a all details of a specific book
@@ -21,7 +20,6 @@ public class ViewCommand extends Command {
             + ": View all details of the book identified by index number in book list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
-    public static final String MESSAGE_NOT_IMPLEMENTED = "Hello from View Command! I'm still under development, please wait :)";
     public static final String MESSAGE_SUCCESS = "Viewing %1$d";
 
     public final Index index;
--- src/main/java/seedu/bookmark/logic/parser/ViewCommandParser.java
+++ src/main/java/seedu/bookmark/logic/parser/ViewCommandParser.java
@@ -1,15 +1,17 @@
 package seedu.bookmark.logic.parser;
 
+import static seedu.bookmark.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.bookmark.commons.core.index.Index;
-import seedu.bookmark.logic.commands.DeleteCommand;
 import seedu.bookmark.logic.commands.ViewCommand;
 import seedu.bookmark.logic.parser.exceptions.ParseException;
 
-import javax.swing.text.View;
-
-import static seedu.bookmark.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
 public class ViewCommandParser implements Parser<ViewCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the ViewCommand
+     * and returns a ViewCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public ViewCommand parse(String input) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(input);
