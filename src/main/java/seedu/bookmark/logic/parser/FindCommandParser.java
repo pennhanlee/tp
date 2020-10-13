7706175eccb6f194ba5fb6e311b2d13c5d6d8b7d
angrybunny123 <kennethteo98@gmail.com>
2020-10-13 21:33:14 +0800

    Edit FindCommandParserTest

--- src/main/java/seedu/bookmark/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/bookmark/logic/parser/FindCommandParser.java
@@ -71,6 +71,11 @@ public class FindCommandParser implements Parser<FindCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
+        if (!trimmedArgs.isEmpty() && (inputPrefix == PREFIX_COMPLETED || inputPrefix == PREFIX_NOT_COMPLETED)) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
         String[] keywords = trimmedArgs.split("\\s+");
 
         Predicate<Book> predicate = null;
