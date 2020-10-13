61d655a79741f0342b727c142ec6a42f7759a8bc
angrybunny123 <kennethteo98@gmail.com>
2020-10-13 20:30:16 +0800

    Edit Find error message

--- src/main/java/seedu/bookmark/logic/commands/FindCommand.java
+++ src/main/java/seedu/bookmark/logic/commands/FindCommand.java
@@ -1,6 +1,11 @@
 package seedu.bookmark.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.bookmark.logic.parser.CliSyntax.PREFIX_NOT_COMPLETED;
+import static seedu.bookmark.logic.parser.CliSyntax.PREFIX_GENRE;
+import static seedu.bookmark.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.bookmark.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.bookmark.logic.parser.CliSyntax.PREFIX_COMPLETED;
 
 import java.util.function.Predicate;
 import seedu.bookmark.commons.core.Messages;
@@ -10,17 +15,23 @@ import seedu.bookmark.model.book.Book;
 
 /**
  * Finds and lists all books in bookmark whose specified property contains any of the argument keywords.
- * Currently, supports finding in name, genre properties, as well as finding completed and non completed books.
+ * Currently supports finding in name, genre and tag properties, as well as finding completed and non completed books.
  * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all books that contain any of the specified "
+            + "keywords in the specified field (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: ONE Prefix Specifier\n"
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_GENRE + "GENRE] "
+            + "[" + PREFIX_TAG + "TAG]... "
+            + "[" + PREFIX_COMPLETED + "COMPLETED BOOKS] "
+            + "[" + PREFIX_NOT_COMPLETED + "BOOKS IN PROGRESS]\n"
+            + "KEYWORD [MORE_KEYWORDS]... (Not needed for c/ and nc/)\n"
+            + "Example: " + COMMAND_WORD + " g/ fantasy horror";
 
     private final Predicate<Book> predicate;
 
