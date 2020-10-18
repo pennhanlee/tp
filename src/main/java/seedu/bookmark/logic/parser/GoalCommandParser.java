225a0b874cdd08c442393636d94e322f1f7ba326
mgiang2015 <mgiang2015@gmail.com>
2020-10-18 15:49:24 +0800

    Update GoalCommandParser

--- src/main/java/seedu/bookmark/logic/parser/GoalCommandParser.java
+++ src/main/java/seedu/bookmark/logic/parser/GoalCommandParser.java
@@ -1,4 +1,44 @@
 package seedu.bookmark.logic.parser;
 
-public class GoalCommandParser {
+import seedu.bookmark.commons.core.index.Index;
+import seedu.bookmark.logic.commands.EditCommand;
+import seedu.bookmark.logic.commands.GoalCommand;
+import seedu.bookmark.logic.parser.exceptions.ParseException;
+import seedu.bookmark.model.book.Goal;
+
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.bookmark.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.bookmark.logic.parser.CliSyntax.PREFIX_DEADLINE;
+import static seedu.bookmark.logic.parser.CliSyntax.PREFIX_PAGE;
+
+public class GoalCommandParser implements Parser<GoalCommand> {
+
+    @Override
+    public GoalCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_PAGE, PREFIX_DEADLINE);
+
+        Index index;
+
+        // Check index
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, GoalCommand.MESSAGE_USAGE), pe);
+        }
+
+        // Check page or date
+        if (argMultimap.getValue(PREFIX_PAGE).isPresent() && argMultimap.getValue(PREFIX_DEADLINE).isPresent()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, GoalCommand.MESSAGE_USAGE));
+        }
+
+        // Both Page and Date should be present
+        String page = argMultimap.getValue(PREFIX_PAGE).get();
+        String deadline = argMultimap.getValue(PREFIX_DEADLINE).get();
+
+        return new GoalCommand(index, new Goal(page, deadline));
+    }
 }
