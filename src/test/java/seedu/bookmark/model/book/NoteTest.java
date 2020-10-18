6584352b46e608d0222635bd498a1a19efaa08d8
pangpuncake <pangpuncake@gmail.com>
2020-10-18 17:29:31 +0800

    Add Test cases for Note

--- src/test/java/seedu/bookmark/model/book/NoteTest.java
+++ src/test/java/seedu/bookmark/model/book/NoteTest.java
@@ -1,4 +1,46 @@
 package seedu.bookmark.model.book;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.bookmark.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class NoteTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Note("Hello", null));
+        assertThrows(NullPointerException.class, () -> new Note(null, "Hello"));
+        assertThrows(NullPointerException.class, () -> new Note(null, null));
+    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalArgumentException() {
+        String invalidName = "";
+        assertThrows(IllegalArgumentException.class, () -> new Name(invalidName));
+    }
+
+    @Test
+    public void isValidNote() {
+        // null text
+        assertThrows(NullPointerException.class, () -> Note.isValidNote("Hello", null));
+        // null title
+        assertThrows(NullPointerException.class, () -> Note.isValidNote(null, "Hello"));
+        // null title and text
+        assertThrows(NullPointerException.class, () -> Note.isValidNote(null, null));
+
+        // invalid note
+        assertFalse(Note.isValidNote("", "")); // empty string
+        assertFalse(Note.isValidNote(" ", " ")); // spaces only
+        assertFalse(Note.isValidNote("^", "^")); // only non-alphanumeric characters
+        assertFalse(Note.isValidNote("peter*", "peter*")); // contains non-alphanumeric characters
+
+        // valid note
+        assertTrue(Note.isValidNote("peter jack", "peter jack")); // alphabets only
+        assertTrue(Note.isValidNote("12345", "12345")); // numbers only
+        assertTrue(Note.isValidNote("peter the 2nd", "peter the 2nd")); // alphanumeric characters
+        assertTrue(Note.isValidNote("Capital Tan", "Capital Tan")); // with capital letters
+        assertTrue(Note.isValidNote("David Roger Jackson Ray Jr 2nd", "David Roger Jackson Ray Jr 2nd")); // long names
+    }
 }
