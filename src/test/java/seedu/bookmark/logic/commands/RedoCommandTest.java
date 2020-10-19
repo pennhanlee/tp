92ff90544119f92d4663cd031b6f3ee357306430
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-10-19 23:28:09 +0800

    Add tests for Undo and Redo command

---
+++ src/test/java/seedu/bookmark/logic/commands/RedoCommandTest.java
@@ -0,0 +1,30 @@
+package seedu.bookmark.logic.commands;
+
+import static seedu.bookmark.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.bookmark.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.bookmark.testutil.TypicalBooks.HARRY_POTTER;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.bookmark.model.Model;
+import seedu.bookmark.model.ModelManager;
+
+public class RedoCommandTest {
+
+    @Test
+    public void undo_nothingToRedo_throwsCommandException() {
+        Model model = new ModelManager();
+
+        assertCommandFailure(new RedoCommand(), model, RedoCommand.MESSAGE_NO_UNDONE_CHANGES);
+    }
+
+    @Test
+    public void redo_validRedo_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+        expectedModel.addBook(HARRY_POTTER);
+        model.addBook(HARRY_POTTER);
+        model.undo();
+        assertCommandSuccess(new RedoCommand(), model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
---
+++ src/test/java/seedu/bookmark/logic/commands/UndoCommandTest.java
@@ -0,0 +1,28 @@
+package seedu.bookmark.logic.commands;
+
+import static seedu.bookmark.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.bookmark.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.bookmark.testutil.TypicalBooks.HARRY_POTTER;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.bookmark.model.Model;
+import seedu.bookmark.model.ModelManager;
+
+public class UndoCommandTest {
+
+    @Test
+    public void undo_nothingToUndo_throwsCommandException() {
+        Model model = new ModelManager();
+
+        assertCommandFailure(new UndoCommand(), model, UndoCommand.MESSAGE_MOST_RECENT_CHANGE);
+    }
+
+    @Test
+    public void undo_validUndo_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+        model.addBook(HARRY_POTTER);
+        assertCommandSuccess(new UndoCommand(), model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
