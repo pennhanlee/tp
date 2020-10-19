b1afc405a3113f2cc96dabb982edfd0c7433d83e
angrybunny123 <kennethteo98@gmail.com>
2020-10-19 17:43:06 +0800

    Add SortCommandTest

--- src/main/java/seedu/bookmark/model/book/comparators/BookPagesReadComparator.java
+++ src/main/java/seedu/bookmark/model/book/comparators/BookPagesReadComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import seedu.bookmark.model.book.Book;
 
 /**
- * Sorts {@code Book}s based on {@code PagesRead} in descending order.
+ * Sorts {@code Book}s based on {@code PagesRead} in ascending order.
  */
 public class BookPagesReadComparator implements Comparator<Book>{
 
@@ -14,7 +14,7 @@ public class BookPagesReadComparator implements Comparator<Book>{
 
         int b2PagesRead = b2.getPagesRead();
 
-        return Integer.compare(b2PagesRead, b1PagesRead);
+        return Integer.compare(b1PagesRead, b2PagesRead);
     }
 
     @Override
