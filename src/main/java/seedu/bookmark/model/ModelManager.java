1732bd2f285b58991040a9e97951fe1b52cfe497
angrybunny123 <kennethteo98@gmail.com>
2020-10-19 13:04:05 +0800

    Add sortFilteredBookList method to ModelManager class

--- src/main/java/seedu/bookmark/model/ModelManager.java
+++ src/main/java/seedu/bookmark/model/ModelManager.java
@@ -132,7 +132,8 @@ public class ModelManager implements Model {
 
     @Override
     public void sortFilteredBookList(Comparator<Book> comparator) {
-
+        requireNonNull(comparator);
+        filteredBooks.sort(comparator);
     }
 
     @Override
