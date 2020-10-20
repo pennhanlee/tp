7d81960310b445f5caf870ddc4fa8559145ee593
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-10-20 21:07:54 +0800

    Add logging when undo and redo is called

--- src/main/java/seedu/bookmark/logic/commands/RedoCommand.java
+++ src/main/java/seedu/bookmark/logic/commands/RedoCommand.java
@@ -1,5 +1,8 @@
 package seedu.bookmark.logic.commands;
 
+import java.util.logging.Logger;
+
+import seedu.bookmark.commons.core.LogsCenter;
 import seedu.bookmark.logic.commands.exceptions.CommandException;
 import seedu.bookmark.model.Model;
 import seedu.bookmark.model.exceptions.RedoException;
@@ -13,13 +16,20 @@ public class RedoCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Previous undone command successfully redone";
     public static final String MESSAGE_NO_UNDONE_CHANGES = "No commands available to redo";
+    private final Logger logger = LogsCenter.getLogger(getClass());
 
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         try {
+            String initialBookList = model.getFilteredBookList().toString();
+            logger.info("Redo initiated, initial books: \n" + initialBookList);
             model.redo();
+            String finalBookList = model.getFilteredBookList().toString();
+            logger.info("Redo success, books modified: " + finalBookList);
         } catch (RedoException e) {
+            String unmodifiedBookList = model.getFilteredBookList().toString();
+            logger.info("Redo failed, books not modified: " + unmodifiedBookList);
             throw new CommandException(MESSAGE_NO_UNDONE_CHANGES);
         }
         return new CommandResult(MESSAGE_SUCCESS);
--- src/main/java/seedu/bookmark/logic/commands/UndoCommand.java
+++ src/main/java/seedu/bookmark/logic/commands/UndoCommand.java
@@ -1,25 +1,35 @@
 package seedu.bookmark.logic.commands;
 
+import java.util.logging.Logger;
+
+import seedu.bookmark.commons.core.LogsCenter;
 import seedu.bookmark.logic.commands.exceptions.CommandException;
 import seedu.bookmark.model.Model;
 import seedu.bookmark.model.exceptions.UndoException;
 
+
 public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undoes the most recent command. \n"
             + "Only commands which modified, added or deleted the Books stored in the Library are considered \n"
             + "Usage: undo";
 
     public static final String MESSAGE_SUCCESS = "Previous command successfully undone";
     public static final String MESSAGE_MOST_RECENT_CHANGE = "No commands available to undo";
+    private final Logger logger = LogsCenter.getLogger(getClass());
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         try {
+            String initialBookList = model.getFilteredBookList().toString();
+            logger.info("Undo initiated, initial books: \n" + initialBookList);
             model.undo();
+            String finalBookList = model.getFilteredBookList().toString();
+            logger.info("Undo success, books modified: " + finalBookList);
         } catch (UndoException e) {
+            String unmodifiedBookList = model.getFilteredBookList().toString();
+            logger.info("Undo failed, books not modified: " + unmodifiedBookList);
             throw new CommandException(MESSAGE_MOST_RECENT_CHANGE);
         }
         return new CommandResult(MESSAGE_SUCCESS);
