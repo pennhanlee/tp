6fa52396674fc48ddb64eb591e22f609912d8b1f
Penn Han <leepennhan98@gmail.com>
2020-10-21 00:07:40 +0800

    Create test for WordComparatorTest.java

--- src/test/java/seedu/bookmark/algo/word/WordComparatorTest.java
+++ src/test/java/seedu/bookmark/algo/word/WordComparatorTest.java
@@ -2,15 +2,56 @@ package seedu.bookmark.algo.word;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.PriorityQueue;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WordComparatorTest {
+    public static final PriorityQueue<Word> differentDistancePQ = new PriorityQueue<>(new WordComparator());
+    public static final PriorityQueue<Word> differentWordsPQ = new PriorityQueue<>(new WordComparator());
+    public static final Word APPLE = new Word("Apple");
+    public static final Word BANANA = new Word("Banana");
+    public static final Word CHERRY = new Word("Cherry");
+    public static final Word DURIAN = new Word("Durian");
+    public static final Word ELDERBERRY = new Word("Elderberry");
+    public static final Word FOO = new Word("Foo", 1);
+    public static final Word BOO = new Word("Boo", 2);
+    public static final Word POO = new Word("Poo", 3);
 
     @Test
-    public void CompareWordCount() {
+    public void CompareWord_ByDistance() {
+        differentDistancePQ.add(FOO);
+        differentDistancePQ.add(BOO);
+        differentDistancePQ.add(POO);
 
+        Word word = differentDistancePQ.poll();
+        assertEquals(word, FOO);
+        word = differentDistancePQ.poll();
+        assertEquals(word, BOO);
+        word = differentDistancePQ.poll();
+        assertEquals(word, POO);
+    }
+
+    @Test
+    public void CompareWord_ByWord() {
+        differentWordsPQ.add(CHERRY);
+        differentWordsPQ.add(DURIAN);
+        differentWordsPQ.add(APPLE);
+        differentWordsPQ.add(BANANA);
+        differentWordsPQ.add(ELDERBERRY);
+
+        Word word = differentWordsPQ.poll();
+        assertEquals(word, APPLE);
+        word = differentWordsPQ.poll();
+        assertEquals(word, BANANA);
+        word = differentWordsPQ.poll();
+        assertEquals(word, CHERRY);
+        word = differentWordsPQ.poll();
+        assertEquals(word, DURIAN);
+        word = differentWordsPQ.poll();
+        assertEquals(word, ELDERBERRY);
     }
 
 }
