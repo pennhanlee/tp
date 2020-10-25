298955579b179c4c404a82e35a896f817d4c13a8
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-10-25 18:36:49 +0800

    Create NoteCard to display Notes

--- src/main/java/seedu/bookmark/ui/DetailedBookListPanel.java
+++ src/main/java/seedu/bookmark/ui/DetailedBookListPanel.java
@@ -3,17 +3,20 @@ package seedu.bookmark.ui;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ListCell;
 import seedu.bookmark.model.book.Book;
+import seedu.bookmark.model.book.Note;
 
 /**
  * Panel that shows detailed information about the books.
  */
 public class DetailedBookListPanel extends BookListPanel {
 
+    private static final String FXML = "DetailedBookListPanel.fxml";
+
     /**
      * Creates a {@code DetailedBookListPanel} with the given {@code ObservableList}.
      */
     public DetailedBookListPanel(ObservableList<Book> bookList) {
-        super();
+        super(FXML);
         bookListView.setItems(bookList);
         bookListView.setCellFactory(lv -> new DetailedBookListViewCell());
     }
@@ -31,6 +34,23 @@ public class DetailedBookListPanel extends BookListPanel {
                 setText(null);
             } else {
                 setGraphic(new DetailedBookCard(book, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the notes of a {@code Book} using a {@code NoteCard}.
+     */
+    class noteListViewCell extends ListCell<Note> {
+        @Override
+        protected void updateItem(Note note, boolean empty) {
+            super.updateItem(note, empty);
+
+            if (empty || note == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new NoteCard(note, getIndex() + 1).getRoot());
             }
         }
     }
