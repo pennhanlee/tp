cdf2cad816f8e381c6638f22b5730eb34e5cde68
Penn Han <leepennhan98@gmail.com>
2020-10-26 18:10:20 +0800

    Set up Interface and Classes for HelpWindow

--- src/main/java/seedu/bookmark/ui/help/HelpAdd.java
+++ src/main/java/seedu/bookmark/ui/help/HelpAdd.java
@@ -1,4 +1,14 @@
 package seedu.bookmark.ui.help;
 
 public class HelpAdd implements HelpAction {
+
+    @Override
+    public String helpIntro() {
+        return null;
+    }
+
+    @Override
+    public String helpMessage() {
+        return null;
+    }
 }
