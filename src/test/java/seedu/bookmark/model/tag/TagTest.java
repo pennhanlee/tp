09afd84ec8f2bad9d3b6cd7cc675c7a1244be497
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2020-10-28 15:42:35 +0800

    Add limits for tags

--- src/test/java/seedu/bookmark/model/tag/TagTest.java
+++ src/test/java/seedu/bookmark/model/tag/TagTest.java
@@ -1,8 +1,10 @@
 package seedu.bookmark.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.bookmark.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import seedu.bookmark.model.book.Name;
 
 public class TagTest {
 
@@ -18,9 +20,25 @@ public class TagTest {
     }
 
     @Test
+    public void consturctor_tooLogTagName_throwsIllegalArgumentException() {
+        StringBuilder tooLong = new StringBuilder();
+        // tooLong is one character too long
+        tooLong.append("a".repeat(Tag.MAX_TAG_LENGTH + 2));
+
+        assertThrows(IllegalArgumentException.class, () -> new Tag(tooLong.toString()));
+    }
+
+    @Test
     public void isValidTagName() {
         // null tag name
         assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
+
+        // too long tag name
+        StringBuilder tooLong = new StringBuilder();
+        // tooLong is one character too long
+        tooLong.append("a".repeat(Tag.MAX_TAG_LENGTH + 2));
+
+        assertFalse(Tag.isValidTagName(tooLong.toString()));
     }
 
 }
